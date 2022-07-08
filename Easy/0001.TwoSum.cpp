49cdfdb2a1628fa64a7de4377eddd6060667730c
ryanshyu <ryan.hsu06190619@gmail.com>
2022-07-08 17:31:06 +0800

    7/8/22

--- Easy/0001.TwoSum.cpp
+++ Easy/0001.TwoSum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-	    int n = nums.size();
         unordered_map<int,int> mpp;
 		vector<int> v;
 		
-		for(int i=0;i<n;i++){ 
-		      if(mpp.find(target - nums[i]) != mpp.end()){
-			      v.push_back(i);
-				  v.push_back(mpp[target-nums[i]]);
-				  return v;
-			  }
-		 mpp[nums[i]] = i;
+		for(int i = 0; i < nums.size(); i++){
+	        if(mpp.find(target - nums[i]) != mpp.end()){
+			    v.push_back(i);
+			    v.push_back(mpp[target - nums[i]]);
+			    return v;
+			}
+            
+		    mpp[nums[i]] = i;
 		 }
 		 
-		 return v;  
+		 return v;
     }
 };

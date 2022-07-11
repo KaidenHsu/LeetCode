ab065f097ebcbac4aa74dfd14a9e04e2d486e81a
ryanshyu <ryan.hsu06190619@gmail.com>
2022-07-11 16:53:49 +0800

    7/11/22

---
+++ 1.Easy/0026.RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,16 @@
+// 7/11/22
+class Solution {
+    public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+
+        int left = 0;
+
+        for(int right = 1; right < nums.size(); right++){
+            if(nums[left] != nums[right]) left++;
+            nums[left] = nums[right];
+        }
+
+        return left + 1;
+    }
+};

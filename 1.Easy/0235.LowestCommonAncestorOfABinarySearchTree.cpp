f93bd6342bff3ffd47c0d2425c5cf415f5212892
ryanshyu <ryan.hsu06190619@gmail.com>
2022-08-12 13:49:16 +0800

    8/12/22

---
+++ 1.Easy/0235.LowestCommonAncestorOfABinarySearchTree.cpp
@@ -0,0 +1,12 @@
+// 8/12/22
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if (p->val < root->val && q->val < root->val) root = root->left;
+            else if (p->val > root->val && q->val > root->val) root = root->right;
+            else return root;
+        }
+        return root;
+    }
+};

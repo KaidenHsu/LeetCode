42077bfc597dd3011221dd11d75c62ed11028a01
ryanshyu <ryan.hsu06190619@gmail.com>
2023-07-29 14:56:26 +0800

    7/29/23

---
+++ 2.Medium/0128.LongestConsecutiveSequence.cpp
@@ -0,0 +1,30 @@
+// 6/29/23
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> set;
+
+        for (int num: nums){
+            set.insert(num);
+        }
+
+        int longestConsecutiveSequence = 0;
+
+        for (int num: nums){
+            if (set.find(num-1) == set.end()){
+                int currentNumber = num;
+                int currentConsecutiveSequence = 1;
+
+                while (set.find(currentNumber+1) != set.end()){
+                    currentNumber++;
+                    currentConsecutiveSequence++;
+                }
+
+                longestConsecutiveSequence = max(longestConsecutiveSequence, currentConsecutiveSequence);
+            }
+        }
+
+        return longestConsecutiveSequence;
+    }
+};
